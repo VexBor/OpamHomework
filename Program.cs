5b7602c91775ab60552ad8d269d2746f030a20cd
Vadim <vadimakim2@gmail.com>
2025-10-14 21:50:46 +0300

    task

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpamHomework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+};
+
